9a2840de659bd2d73f24025f2d0a7b7d8629f4c8
abderraouf-taibi <raouftaibi240@gmail.com>
2025-05-04 11:09:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,35 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
-
-import { AppProvider } from './context/AppContext';
-const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {import React from 'react';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
-import Budget from './components/Budget';
 import ExpenseItem from './components/ExpenseItem';
 import ExpenseList from './components/ExpenseList';
 import Remaining from './components/Remaining';
@@ -93,23 +64,6 @@ const App = () => {
                         }    
 
                 
-            </div>
-        </AppProvider>
-    );
-};
-export default App;
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
-                </div>
             </div>
         </AppProvider>
     );
